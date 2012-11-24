3283d7013211d61eb59e41ea207cb521523fbafe
jane <gemini0707@gmail.com>
2012-11-24 10:13:17 -0800

    delete add note to priority migration.

--- db/migrate/20091104192606_add_note_to_priorities.rb
+++
@@ -1,9 +0,0 @@
-class AddNoteToPriorities < ActiveRecord::Migration
-  def self.up
-    add_column :priorities, :note, :text
-  end
-
-  def self.down
-    remove_column :priorities, :note
-  end
-end
