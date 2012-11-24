5cb19f14373d3d5843917e60a30e018840e844c4
jane <gemini0707@gmail.com>
2012-11-24 10:42:55 -0800

    sync case id type to integer migration.

--- db/migrate/20100219234345_fix_reminder_patentcase_join.rb
+++ db/migrate/20100219234345_fix_reminder_patentcase_join.rb
@@ -1,11 +1,11 @@
 class FixReminderPatentcaseJoin < ActiveRecord::Migration
   def self.up
+    change_column :reminders, :case_id, :integer
     rename_column :reminders, :case_id, :patentcase_id
-    change_column :reminders, :patentcase_id, :integer
   end
 
   def self.down
-    change_column :reminders, :patentcase_id, :string
     rename_column :reminders, :patentcase_id, :case_id
+    change_column :reminders, :case_id, :string
   end
 end
