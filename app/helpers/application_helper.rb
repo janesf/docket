f76b4cbd35f5266d82e8a71affd49bb823baa77d
jane <gemini0707@gmail.com>
2012-11-23 23:07:50 -0800

    add header board.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on   IveyLaw.org"
+    base_title = "Docket Management - IveyLaw.org"
     if @title.nil?
       base_title
     else
