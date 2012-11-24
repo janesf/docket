924bf2e82d7e7ce71abec2487bab386a3a169c2f
jane <gemini0707@gmail.com>
2012-11-24 10:21:08 -0800

    add user table mods migrations.

--- db/migrate/20100822204528_add_password_to_users.rb
+++ db/migrate/20100822204528_add_password_to_users.rb
@@ -1,9 +1,13 @@
 class AddPasswordToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :encrypted_password, :string
+    add_column :users, :salt, :string
+    
   end
 
   def self.down
     remove_column :users, :encrypted_password
+    remove_column :users, :salt
+    
   end
 end
