f3f316311902e3b9e421da53d4d01fcffcbdd45b
jane <gemini0707@gmail.com>
2012-11-29 13:36:08 -0800

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
 #  before_save :create_remember_token
   before_save :encrypt_password
-
+  validates :role_id, presence: true
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
